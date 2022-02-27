ddef06d8dffb8a4ba8278407c098545e60391653
Simeon Umoh <simeoneumoh@outlook.com>
2022-02-27 10:23:28 +0100

    add_view

--- Product_Importer/products/views.py
+++ Product_Importer/products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .tasks import add
 
-# Create your views here.
+
+@api_view(['GET', 'PUT', 'POST'])
+def add_view(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'POST':
+        request_data = request.data
+        x = request_data['x']
+        y = request_data['y']
+
+        result = add.delay(x,y)
+        return Response({"result": result })
