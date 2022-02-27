a950a6679324ac3f732e619166f5a2798dcf4d75
Simeon Umoh <simeoneumoh@outlook.com>
2022-02-27 01:48:01 +0100

    SETTINGS

--- Product_Importer/config/settings.py
+++ Product_Importer/config/settings.py
@@ -149,6 +149,7 @@ CORS_ALLOW_METHODS = [
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
