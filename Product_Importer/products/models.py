1b9c3fff04573f4415559525fea56851c2a8069b
Simeon Umoh <simeoneumoh@outlook.com>
2022-02-27 10:38:40 +0100

    Products models updates

--- Product_Importer/products/models.py
+++ Product_Importer/products/models.py
@@ -1,3 +1,10 @@
+from re import T
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    name = models.TextField(null=True)
+    sku = models.TextField(null=True, unique=True)
+    description = models.TextField(null=True)
+    
+    created_at = models.DateTimeField("created_at", auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
