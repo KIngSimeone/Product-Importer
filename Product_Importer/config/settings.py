75da2a532c60a2cc3eafcf40e63167748c88c1a4
Simeon Umoh <simeoneumoh@outlook.com>
2022-02-27 10:12:15 +0100

    CELERY_BROKER_URL

--- Product_Importer/config/settings.py
+++ Product_Importer/config/settings.py
@@ -158,7 +158,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CELERY STUFF
-BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
